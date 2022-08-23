76ad0f1625e54d9819380e95468910298d4013fa
Carlos Alves <carlosalves369m@hotmail.com>
2022-08-23 16:42:57 -0300

    feat: criando e usando highlight lib para terminal shell

---
+++ libs/main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source $HOME/init/utils.sh
+
+e_header "I am a sample script"
+e_success "I am a success message"
+e_error "I am an error message"
+e_warning "I am a warning message"
+e_underline "I am underlined text"
+e_bold "I am bold text"
+e_note "I am a note"
